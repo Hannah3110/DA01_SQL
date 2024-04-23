330ca8d0e1005ba78fe83dea03c5a9236fbf59f8
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-04-23 16:09:39 +0900

    Update DAY5_EXERCISES.sql

--- DAY5_EXERCISES.sql
+++ DAY5_EXERCISES.sql
@@ -24,3 +24,21 @@ where post_date>='2021-01-01' and post_date<'2022-01-01'
 group by user_id
 having count(post_id)>=2
 --Day5_Exercise7
+SELECT card_name, 
+max(issued_amount)-min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
+order by difference DESC
+--Day5_Exercise8
+SELECT manufacturer,
+count(drug) as drug_count,
+abs(sum(cogs-total_sales)) as total_loss
+FROM pharmacy_sales
+where total_sales<cogs
+group by manufacturer
+order by total_loss DESC
+--Day5_Exercise9
+select * from Cinema
+where id%2=1 and description <> 'boring'
+order by rating DESC
+--Day5_Exercise10
