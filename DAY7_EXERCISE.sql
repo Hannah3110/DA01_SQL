43ef2d2fa233cd324f9eab1babb2021275da81ea
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-04-25 11:53:16 +0900

    Update DAY7_EXERCISE.sql

--- DAY7_EXERCISE.sql
+++ DAY7_EXERCISE.sql
@@ -9,3 +9,9 @@ concat(upper(left(name,1)),lower(right(name,length(name)-1))) as name
 from Users
 order by user_id
 --Day7_exercise3
+select manufacturer,
+concat('$',round(sum(total_sales)/1000000,0),' ','million') as sale
+from pharmacy_sales
+group by manufacturer
+order by sum(total_sales) DESC,manufacturer
+--Day7_exercise4
