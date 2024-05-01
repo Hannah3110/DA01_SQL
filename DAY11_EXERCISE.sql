b3daa77c22606ae0603f6824d8f761a1e253c39d
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-01 15:12:34 +0900

    Update DAY11_EXERCISE.sql

--- DAY11_EXERCISE.sql
+++ DAY11_EXERCISE.sql
@@ -26,4 +26,29 @@ INNER JOIN age_breakdown AS b
 ON a.user_id=b.user_id
 where activity_type in ('send','open')
 group by age_bucket
---Day11_Exercise3
+--Day11_Exercise4
+SELECT 
+a.customer_id
+FROM customer_contracts as a   
+INNER JOIN products as b  
+ON a.product_id=b.product_id
+group by a.customer_id
+having count(distinct b.product_category)=3
+--Day11_Exercise5
+select mng.employee_id, mng.name,
+count(emp.employee_id) as reports_count,
+round(avg(emp.age)) as average_age
+from Employees as emp
+join Employees as mng
+on mng.employee_id=emp.reports_to 
+group by employee_id
+order by employee_id
+--Day11_Exercise6
+select a.product_name, sum(b.unit) as unit
+from Products as a
+inner join Orders as b
+on a.product_id=b.product_id
+where extract(month from b.order_date)=02 and extract(year from b.order_date)=2020
+group by a.product_name
+having sum(b.unit)>=100
+--Day11_Exercise6
