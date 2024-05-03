bd40681e4a2cea34370e4999b85ec1d434bfd19e
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-03 23:19:06 +0900

    Update DAY13_EXERCISE.sql

--- DAY13_EXERCISE.sql
+++ DAY13_EXERCISE.sql
@@ -1,3 +1,4 @@
+--Day13_Exercise1
 WITH counting_job_duplicate as 
 (SELECT company_id, title, description,
 count(job_id) as job_count 
@@ -7,3 +8,26 @@ group by company_id, title, description)
 select count(company_id) as duplicate_companies
 from counting_job_duplicate
 where job_count > 1
+--Day13_Exercise2
+with cte_cat1 as
+(SELECT category, product, sum(spend) as total_spend
+FROM product_spend
+where extract (year from transaction_date)=2022
+and category='appliance'
+group by category, product
+order by sum(spend) DESC
+limit 2),
+cte_cat2 as
+(SELECT category, product, sum(spend) as total_spend
+FROM product_spend
+where extract (year from transaction_date)=2022
+and category='electronics'
+group by category, product
+order by sum(spend) DESC
+limit 2)
+
+select category, product, total_spend
+from cte_cat1
+UNION ALL 
+select category, product, total_spend
+from cte_cat2
