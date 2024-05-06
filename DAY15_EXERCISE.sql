029f7f3ac10cfe260ec3815fb9e8603b78b224c0
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-06 13:14:31 +0900

    Update DAY15_EXERCISE.sql

--- DAY15_EXERCISE.sql
+++ DAY15_EXERCISE.sql
@@ -9,3 +9,17 @@ lag(spend) over(PARTITION BY product_id
 order by extract(year from transaction_date)),2) as yoy_rate
 FROM user_transactions
 --Day15_EX2
+select card_name,issued_amount from
+(SELECT card_name,issued_amount,
+rank() over(partition by card_name order by issue_year, issue_month)
+as first_month  
+FROM monthly_cards_issued) as a   
+where first_month = 1
+order by issued_amount DESC
+--Day15_EX3  
+select user_id, spend, transaction_date FROM
+(SELECT user_id, spend, transaction_date,
+row_number() over(PARTITION BY user_id order by transaction_date) as trans_date
+FROM transactions) as a
+where trans_date=3
+--Day15_EX4
