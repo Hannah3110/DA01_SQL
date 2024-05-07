3f6f36958b0718d01c95212dda2d40639e5fb746
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-07 21:52:12 +0900

    Update DAY16_EXERCISE.sql

--- DAY16_EXERCISE.sql
+++ DAY16_EXERCISE.sql
@@ -12,3 +12,23 @@ round(100.0*sum(case
     else 0
     end)/count(immediate),2) as immediate_percentage
 from cte
+--Day16_Ex7
+with cte as
+(select b.name as Department,a.name as Employee,a.salary as Salary,
+dense_rank() over(partition by b.name order by a.salary desc) as rank_order
+from Employee as a
+join Department as b
+on a.departmentId=b.id)
+select Department,Employee,Salary
+from cte
+where rank_order<=3
+--Day16_Ex7
+with cte as
+(select person_name,
+sum(weight) over(order by turn) as total_weight
+from Queue)
+select person_name
+from cte
+where total_weight<=1000
+order by total_weight desc
+limit 1
