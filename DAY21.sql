ba3cdb0e3705e5cd3ea83f105d260fb4f71759f4
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-11 21:06:46 +0900

    Update DAY21.sql

--- DAY21.sql
+++ DAY21.sql
@@ -11,8 +11,8 @@ Group by month_year
 ORDER BY month_year;
 ----Ex2
 select FORMAT_DATE('%Y-%m', t2.delivered_at) as month_year, 
-count(DISTINCT t1.user_id) as total_user,
-avg(t2.sale_price) as avg_sale
+count(DISTINCT t1.user_id) as distinct_users,
+avg(t2.sale_price) as avg_order_value
 from bigquery-public-data.thelook_ecommerce.orders as t1
 Join bigquery-public-data.thelook_ecommerce.order_items as t2 
 on t1.order_id=t2.order_id
@@ -20,3 +20,11 @@ Where t1.status='Complete' and
 t2.delivered_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
 Group by month_year
 ORDER BY month_year
+----Ex3
+select * from
+(select first_name, last_name,gender,age,
+case when age=min(age) over(partition by gender order by age) then 'youngest'
+    when age=max(age) over(partition by gender order by age) then 'oldest'
+end as tag
+from bigquery-public-data.thelook_ecommerce.users) as a
+where tag in ('youngest','oldest')
