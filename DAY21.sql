0680335bf1974073891a36c927255836d617f6fe
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-11 20:34:38 +0900

    Create DAY21.sql

---
+++ DAY21.sql
@@ -0,0 +1,22 @@
+----Ex1
+select FORMAT_DATE('%Y-%m', t2.delivered_at) as month_year, 
+count(DISTINCT t1.user_id) as total_user,
+count(t1.ORDER_id) as total_order
+from bigquery-public-data.thelook_ecommerce.orders as t1
+Join bigquery-public-data.thelook_ecommerce.order_items as t2 
+on t1.order_id=t2.order_id
+Where t1.status='Complete' and 
+t2.delivered_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+Group by month_year
+ORDER BY month_year;
+----Ex2
+select FORMAT_DATE('%Y-%m', t2.delivered_at) as month_year, 
+count(DISTINCT t1.user_id) as total_user,
+avg(t2.sale_price) as avg_sale
+from bigquery-public-data.thelook_ecommerce.orders as t1
+Join bigquery-public-data.thelook_ecommerce.order_items as t2 
+on t1.order_id=t2.order_id
+Where t1.status='Complete' and 
+t2.delivered_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+Group by month_year
+ORDER BY month_year
