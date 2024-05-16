59706fb1419e9e052542b41107102c44e45e0b63
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-16 18:13:08 +0900

    Update Day24_Project3.sql

--- Day24_Project3.sql
+++ Day24_Project3.sql
@@ -1,5 +1,16 @@
+---ex1
 select productline, year_id, dealsize,
 sum(sales) as revenue
 from public.sales_dataset_rfm_prj
 group by productline, year_id, dealsize
 order by year_id, productline,dealsize
+---ex2
+select year_id, month_id,
+revenue from
+(select year_id,MONTH_ID, 
+sum(sales) as revenue,
+dense_rank() over (partition by year_id order by sum(sales) DESC) as rank_order
+from public.sales_dataset_rfm_prj
+group by year_id,month_id
+order by year_id,month_id) as a
+where rank_order =1
