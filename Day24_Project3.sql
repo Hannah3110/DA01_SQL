beb874179c82c182dbac98a3bb68857abc0ea154
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-16 19:05:49 +0900

    Update Day24_Project3.sql

--- Day24_Project3.sql
+++ Day24_Project3.sql
@@ -37,45 +37,6 @@ order by year_id) as a
 where rank =1
 order by revenue
 ---ex5
-select productline, year_id, dealsize,
-sum(sales) as revenue
-from public.sales_dataset_rfm_prj
-group by productline, year_id, dealsize
-order by year_id, productline,dealsize;
-
-select year_id, month_id,
-revenue from
-(select year_id,MONTH_ID, 
-sum(sales) as revenue,
-dense_rank() over (partition by year_id order by sum(sales) DESC) as rank_order
-from public.sales_dataset_rfm_prj
-group by year_id,month_id
-order by year_id,month_id) as a
-where rank_order =1;
-
-select year_id,month_id,productline,
-revenue from
-(select year_id,month_id,productline,
-sum(sales) as revenue,
-dense_rank() over (partition by year_id order by sum(sales) DESC) as rank_order
-from sales_dataset_rfm_prj
-where month_id='11'
-group by year_id,month_id, productline
-order by year_id,month_id, productline) as a
-where rank_order=1;
-
-select * from
-(select year_id,productline,
-sum(sales) as revenue,
-row_number() over (partition by year_id order by sum(sales) DESC) as rank
-from sales_dataset_rfm_prj
-where country='UK'
-group by year_id,productline
-order by year_id) as a
-where rank =1
-order by revenue;
-select * from sales_dataset_rfm_prj;
-
 with customer_RFM as
 (select customername,
 extract (days from (current_date-max(orderdate))) as R,
