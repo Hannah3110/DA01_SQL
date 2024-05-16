02e7941e1f6706e3ed0a414a9ece5d162cb1a83c
Hannah3110 <167667468+Hannah3110@users.noreply.github.com>
2024-05-16 18:35:15 +0900

    Update Day24_Project3.sql

--- Day24_Project3.sql
+++ Day24_Project3.sql
@@ -25,3 +25,14 @@ where month_id='11'
 group by year_id,month_id, productline
 order by year_id,month_id, productline) as a
 where rank_order=1
+---ex4
+select * from
+(select year_id,productline,
+sum(sales) as revenue,
+row_number() over (partition by year_id order by sum(sales) DESC) as rank
+from sales_dataset_rfm_prj
+where country='UK'
+group by year_id,productline
+order by year_id) as a
+where rank =1
+order by revenue
